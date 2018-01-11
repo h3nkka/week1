1cb0577cabd318132c40cb307fbb8f86836318a4
K8948 <K8948@student.jamk.fi>
2018-01-11 18:44:18 +0200

    Add project files.

---
+++ ex2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input points as integer:");
+            int input = Int32.Parse(Console.ReadLine());
+            switch (input)
+            {
+                case 0:
+                case 1:
+                    Console.WriteLine("School grade is 0");
+                    break;
+                case 2:
+                case 3:
+                    Console.WriteLine("School grade is 1");
+                    break;
+                case 4:
+                case 5:
+                    Console.WriteLine("School grade is 2");
+                    break;
+                case 6:
+                case 7:
+                    Console.WriteLine("School grade is 3");
+                    break;
+                case 8:
+                case 9:
+                    Console.WriteLine("School grade is 4");
+                    break;
+                case 10:
+                case 11:
+                case 12:
+                    Console.WriteLine("School grade is 5");
+                    break;
+            }
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ week1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number between 1 and 3");
+            int input = Int32.Parse(Console.ReadLine());
+            if (input == 1)
+                Console.WriteLine("Number is one");
+            else if (input == 2)
+                Console.WriteLine("Number is two");
+            else if (input == 3)
+                Console.WriteLine("Number is three");
+            else
+                Console.WriteLine("Some other number");
+            Console.ReadKey(true);
+        }
+    }
+}
